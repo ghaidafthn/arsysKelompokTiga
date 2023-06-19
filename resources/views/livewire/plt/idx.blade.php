251f4488b9c5a81ca6b5ca48d1741ddcfb91a84a
ghaidafthn <89296974+ghaidafthn@users.noreply.github.com>
2023-06-19 10:45:15 +0700

    Update idx.blade.php

--- resources/views/livewire/plt/idx.blade.php
+++ resources/views/livewire/plt/idx.blade.php
@@ -1,76 +1,174 @@
-<div>
-    @section('content')
-        <div>
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="text-center mb-2">
-                                <img src="http://localhost/ArSysPure/resources/img/Profile Image.jpg" class="img-thumbnail rounded-circle mb-2" alt="Profile Picture" width="150" style="object-fit: cover; width: 150px; height: 150px;">
-                                    <h4 class="font-weight-bold">Rifki Alfian Nahar</h4>
-                                </div>
-                                <div>
-                                    <h4 class="">About</h4>
-                                    <p class="card-text text-justify">Rifki Alfian Nahar Mahasiswa Teknik Elektro UPI</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="container">    
-                        <div class="row">
-                            <div class="col">
-                                <div class="card">
-                                    <div class="card-header bg-gray">
-                                        <h3 class="font-weight-bolder">Profile</h3>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col">
-                                                <h6 class="mb-0
-                                                ">Nama</h6>
-                                            </div>
-                                            <div class="col text-secondary">
-                                                <p>Rifki Alfian Nahar</p>
-                                            </div>
-                                        </div>
-                                    <hr>
-                            
-                                        <div class="row">
-                                            <div class="col">
-                                                <h6 class="mb-0">NIM</h6>
-                                            </div>
-                                            <div class="col text-secondary">
-                                                <p>2003788</p>
-                                            </div>
-                                        </div>
-                                    <hr>
-                                    
-                                        <div class="row">
-                                            <div class="col">
-                                                <h6 class="mb-0">Program Studi</h6>
-                                            </div>
-                                            <div class="col text-secondary">
-                                                <p>S1 - Teknik Elektro</p>
-                                            </div>
-                                        </div>
-                                    <hr>
-                                    
-                                        <div class="row">
-                                            <div class="col">
-                                                <h6 class="mb-0">Email</h6>
-                                            </div>
-                                            <div class="col text-secondary">
-                                                <p>rifkinahar@upi.edu</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>    
-                            </div>  
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endsection
+@section('content')
+        
+    
+<div class="card-header">
+  <b>Event</b> | Schedule of research defense and seminar
 </div>
+
+<div class="col-md-12 offset-sm-0">
+<div class="table-responsive users-table">
+    <table class="table table-striped table-sm data-table">
+       <thead class="thead">
+        <tr>
+            <th width="45%">Event Name</th>
+            <th width="15%">Event Date</th>
+            <th width="15%">Application Deadline</th>
+            <th width="15%">Turnitin Deadline</th>
+            <th class="text-center" width="10%">Seats left</th>
+
+        </tr>
+       </thead>
+    <tbody id="users-table">
+        <tr>
+          <td>
+            Event
+             <br>
+                Seminar 1
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+
+          <td class="text-center">
+            0
+          </td>
+
+        </tr>
+                                       
+        <tr>
+          <td>
+            Event
+             <br>
+                Seminar 2
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+
+          <td class="text-center">
+            0
+          </td>
+
+        </tr>
+
+        <tr>
+          <td>
+            Event
+             <br>
+                Seminar 3
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+
+          <td class="text-center">
+            0
+          </td>
+
+        </tr>
+
+        <tr>
+          <td>
+            Event
+             <br>
+                Seminar 4
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+
+          <td class="text-center">
+            0
+          </td>
+
+        </tr>
+
+        <tr>
+          <td>
+            Event
+             <br>
+                Seminar 5
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+          <td>
+            Day, <br>
+            Date <br>
+            Time <br>
+          </td>
+
+          <td class="text-center">
+            0
+          </td>
+
+        </tr>
+                                       
+                                                    </tbody>
+                    </table>
+                </div>
+                <div>
+                    
+        <nav>
+            
+        </nav>
+    </div>
+
+            
+        </div>
+                @endsection
